19e875e669d85da5914dd08e8c8fc79e27665261
Olezha228 <olegfide@mail.ru>
2024-01-10 19:05:24 +0500

    changed

--- MilanPro/src/Bookify.Domain/obj/Debug/net8.0/Bookify.Domain.AssemblyInfo.cs
+++ MilanPro/src/Bookify.Domain/obj/Debug/net8.0/Bookify.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bookify.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f521cc5fc05d567d175cbacb44fe81433e1367af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bookify.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bookify.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
